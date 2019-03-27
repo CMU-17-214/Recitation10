0add5d68c0e56ac3e3718c3ef9be13aa42b4535e
Yanqing Wang <wangyanqingchn@163.com>
2019-03-27 14:06:00 -0400

    address a comment

--- src/main/java/edu/cmu/cs/cs214/rec10/plugin/SortWithSwaps.java
+++ src/main/java/edu/cmu/cs/cs214/rec10/plugin/SortWithSwaps.java
@@ -7,7 +7,7 @@ import edu.cmu.cs.cs214.rec10.framework.core.GamePlugin;
 import java.util.Random;
 
 public class SortWithSwapsPlugin implements GamePlugin {
-    private int ONE = 1;
+    private static final int ONE = 1;
     private int TEN = 10;
 
     // Describes whether the current move is over
