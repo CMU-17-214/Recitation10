5f1693049e1c047a78c2b36244fc7044a6211779
clevercurlcurl <1608815224@qq.com>
2019-10-30 09:57:16 -0400

    change * to HashMap and Random

--- src/main/java/edu/cmu/cs/cs214/rec10/plugin/MoneyDoorPlugin.java
+++ src/main/java/edu/cmu/cs/cs214/rec10/plugin/MoneyDoorPlugin.java
@@ -4,7 +4,8 @@ import edu.cmu.cs.cs214.rec10.framework.core.GameFramework;
 import edu.cmu.cs.cs214.rec10.framework.core.GamePlugin;
 import edu.cmu.cs.cs214.rec10.framework.core.Player;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Random;
 
 public class MoneyDoorPlugin implements GamePlugin {
 
